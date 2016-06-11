0d80f1e1b08a2178ce497a40111c7c97e9fac9ad
Geoff McLane <ubuntu@geoffair.info>
2016-06-11 15:19:38 +0200

    Issue #2 - get closer to console app type

--- src/test-con2.c
+++ src/test-con2.c
@@ -15,17 +15,19 @@
 
 static const char *module = "test-con2";
 
+// add shorter, clearer message of 'type'
 typedef struct tagDW2STG {
     DWORD val;
+    char *type;
     char *desc;
 }DW2STG, *PDW2STG;
 
 DW2STG ft2stg[] = {
-    { FILE_TYPE_CHAR, "is a character file, typically an LPT device or a console." },
-    { FILE_TYPE_DISK, "is a disk file." },
-    { FILE_TYPE_PIPE, "is a socket, a named pipe, or an anonymous pipe." },
-    { FILE_TYPE_REMOTE, "Unused." },
-    { FILE_TYPE_UNKNOWN, "Either the type of the specified file is unknown, or the function failed." },
+    { FILE_TYPE_CHAR, "FILE_TYPE_CHAR", "is a character file, typically an LPT device or a console." },
+    { FILE_TYPE_DISK, "FILE_TYPE_DISK", "is a disk file." },
+    { FILE_TYPE_PIPE, "FILE_TYPE_PIPE", "is a socket, a named pipe, or an anonymous pipe." },
+    { FILE_TYPE_REMOTE, "FILE_TYPE_REMOTE", "Unused." },
+    { FILE_TYPE_UNKNOWN, "FILE_TYPE_UNKNOWN", "Either the type of the specified file is unknown, or the function failed." },
 
     // LAST
     { 0, 0 }
@@ -36,7 +38,7 @@ char *GetFileTypeStg( DWORD dw )
     PDW2STG pdw = &ft2stg[0];
     while (pdw->desc) {
         if (pdw->val == dw )
-            return pdw->desc;
+            return pdw->type;   // pdw->desc;
         pdw++;
     }
     return "Unexpected type";
@@ -50,49 +52,63 @@ static void log_output( int n )
     fflush(stderr);
 }
 
-
+// main app entry
 int main( int argc, char **argv )
 {
     int iret = 0;
     int attached = 0;
+    // get 'stdout' info
     HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
     DWORD type = GetFileType(hout);
     char *pft = GetFileTypeStg(type);
+    // get 'stderr' info
+    HANDLE herr = GetStdHandle(STD_ERROR_HANDLE);
+    DWORD etyp = GetFileType(herr);
+    char *pfte = GetFileTypeStg(etyp);
     char *nb = GetNxtBuf();
     DWORD err;
     char *pmsg;
     int len = sprintf(nb, "HOut is %s (type %d) - handle: %p (%d)", pft, type, hout, STD_OUTPUT_HANDLE);
+    len += sprintf(EndBuf(nb), "\nHErr is %s (type %d) - handle: %p (%d)", pfte, etyp, herr, STD_ERROR_HANDLE);
 
     // ##################################
     log_output(1);    // 1
     // ##################################
 
     if (AttachConsole(ATTACH_PARENT_PROCESS)) {
-        int dnfro = 0;
+        int dnfro1 = 0;
+        int dnfro2 = 0;
         // ##################################
         log_output(2);  // 2
         // ##################################
-        if (type == FILE_TYPE_CHAR) {
+
+        // selectively use 'freopen', if not already REDIRECTED!
+        if (type != FILE_TYPE_DISK) {
             freopen("conout$", "w", stdout);
-            freopen("conout$", "w", stderr);
-            dnfro = 1;
+            dnfro1 = 1;
         }
-        SPRTF("%s: AttachConsole succeeded... %s\n", module,
-            (dnfro ? "Done fropen" : "not disk file"));
+        if (etyp != FILE_TYPE_DISK) {
+            freopen("conout$", "w", stderr);
+            dnfro2 = 1;
+        }
+        SPRTF("\n%s: AttachConsole succeeded... stdout: %s, stderr: %s\n", module,
+            (dnfro1 ? "Done fropen" : "is disk file"),
+            (dnfro2 ? "Done fropen" : "is disk file"));
         attached = 1;
     } else {
+        // obviously **NOT** run in a 'console'
         err = GetLastError();
         pmsg = (err == ERROR_ACCESS_DENIED) ? "ERROR_ACCESS_DENIED" :
             (err == ERROR_INVALID_HANDLE) ? "ERROR_INVALID_HANDLE" :
             (err == ERROR_GEN_FAILURE) ? "ERROR_GEN_FAILURE" :
             "UNEXPECTED_ERROR";
 
-        SPRTF("%s: AttachConsole FAILED... %s (%d)\n", module, pmsg, err );
+        SPRTF("\n%s: AttachConsole FAILED... %s (%d)\n", module, pmsg, err );
         iret = 1;
     }
 
     // do some logging...
-    SPRTF("%s: %s\n", module, nb );
+    SPRTF("Info:\n%s\n", nb );
     // ##################################
     log_output(3); // 3
     // ##################################
@@ -117,7 +133,7 @@ int main( int argc, char **argv )
     return iret;
 }
 
-// main() OS entry
+// OS entry, when SUBSYSTEM:WINDOWS is used
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     int iret;
