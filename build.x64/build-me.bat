e55f9c65f8ad2257cc527cdb0fbcd6d790f3f425
Geoff McLane <ubuntu@geoffair.info>
2017-05-22 15:57:35 +0200

    Build with MSVC14 2015

--- build.x64/build-me.bat
+++ build.x64/build-me.bat
@@ -1,11 +1,12 @@
 @setlocal
+@set VCVERS=14
 @set TMPPRJ=test_console
 @echo Build %TMPPRJ% project, in 64-bits
 @set TMPLOG=bldlog-1.txt
 @set BLDDIR=%CD%
-@set BLDDBG=0
+@set BLDDBG=1
 @set TMPROOT=..
-@set SET_BAT=%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat
+@set SET_BAT=%ProgramFiles(x86)%\Microsoft Visual Studio %VCVERS%.0\VC\vcvarsall.bat
 @if NOT EXIST "%SET_BAT%" goto NOBAT
 @if NOT EXIST %TMPROOT%\nul goto NOROOT
 @set TMPSRC=..
@@ -24,7 +25,7 @@
 :DNARCH
 @set TMPOPTS
 @REM set TMPOPTS=-DCMAKE_INSTALL_PREFIX=%TMPINST%
-@set TMPOPTS=%TMPOPTS% -G "Visual Studio 10 Win64"
+@set TMPOPTS=%TMPOPTS% -G "Visual Studio %VCVERS% Win64"
 
 :RPT
 @if "%~1x" == "x" goto GOTCMD
