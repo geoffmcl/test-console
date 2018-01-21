2e458ddb5acd0b96918e59d53c85fd0a1e8be7a8
Geoff McLane <ubuntu@geoffair.info>
2018-01-21 18:38:02 +0100

    Attach to console, if it exists.
    test-con3 is a Windows app, so normally has no 'console', and debug output
    to 'stdout' goes nowhere.
    
    But if the app is started in a 'console', a command prompt, attach to that
    console and freopen conout$ so the ouput appears there.
    
    And if an ouput redirection is added, then allow that to exist and avoid
    the freopen which will break that redirection. ie bRedON=TRUE
    
    	modified:   src/Win32/test-con3.cpp

--- src/Win32/test-con3.cpp
+++ src/Win32/test-con3.cpp
@@ -7,6 +7,7 @@
 \*/
 
 #include "test-con3.h"
+#include <stdio.h>  // for fopen, stdout, ...
 #include "..\sprtf.hxx"
 
 #define MAX_LOADSTRING 100
@@ -24,17 +25,100 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+// add shorter, clearer message of 'type'
+typedef struct tagDW2STG {
+    DWORD val;
+    char *type;
+    char *desc;
+}DW2STG, *PDW2STG;
+
+static DW2STG ft2stg[] = {
+    { FILE_TYPE_CHAR, "FILE_TYPE_CHAR", "is a character file, typically an LPT device or a console." },
+    { FILE_TYPE_DISK, "FILE_TYPE_DISK", "is a disk file." },
+    { FILE_TYPE_PIPE, "FILE_TYPE_PIPE", "is a socket, a named pipe, or an anonymous pipe." },
+    { FILE_TYPE_REMOTE, "FILE_TYPE_REMOTE", "Unused." },
+    { FILE_TYPE_UNKNOWN, "FILE_TYPE_UNKNOWN", "Either the type of the specified file is unknown, or the function failed." },
+
+    // LAST
+    { 0, 0 }
+};
+
+static char *GetFileTypeStg(DWORD dw)
+{
+    PDW2STG pdw = &ft2stg[0];
+    while (pdw->desc) {
+        if (pdw->val == dw)
+            return pdw->type;   // pdw->desc;
+        pdw++;
+    }
+    return "Unexpected type";
+}
+
+// See Reference
+static void sendEnterKey(void)
+{
+    INPUT ip;
+    // Set up a generic keyboard event. 
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.wScan = 0; // hardware scan code for key 
+    ip.ki.time = 0;
+    ip.ki.dwExtraInfo = 0;
+    // Send the "Enter" key 
+    ip.ki.wVk = 0x0D; // virtual-key code for the "Enter" key 
+    ip.ki.dwFlags = 0; // 0 for key press 
+    SendInput(1, &ip, sizeof(INPUT));
+    // Release the "Enter" key 
+    ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release 
+    SendInput(1, &ip, sizeof(INPUT));
+}
+
+int attach_console()
+{
+    int iret = 0;
+    DWORD dwMode = 0;
+    BOOL bRedON = FALSE;
+    // get 'stdout' info
+    HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);  // get stdout handle
+    DWORD type = GetFileType(hout);
+    char *desc = GetFileTypeStg(type);
+    if (hout && !GetConsoleMode(hout, &dwMode))
+        bRedON = TRUE;
+
+    if (AttachConsole(ATTACH_PARENT_PROCESS)) {
+        int dnfro1 = 0;
+        // selectively use 'freopen', if not already REDIRECTED!
+        if ((type != FILE_TYPE_DISK) && !bRedON) {
+            freopen("conout$", "w", stdout);
+            dnfro1 = 1;
+        }
+        SPRTF("\nAttachConsole succeeded... stdout: type %s, %s, bRedON=%s\n", desc, 
+            (dnfro1 ? "done fropen" : "is disk file"),
+            (bRedON ? "TRUE" : "FALSE") );
+    }
+    else {
+        DWORD err = GetLastError();
+        const char *pmsg = (err == ERROR_ACCESS_DENIED) ? "ERROR_ACCESS_DENIED" :
+            (err == ERROR_INVALID_HANDLE) ? "ERROR_INVALID_HANDLE" :
+            (err == ERROR_GEN_FAILURE) ? "ERROR_GEN_FAILURE" :
+            "UNEXPECTED_ERROR";
+        SPRTF("\nAttachConsole FAILED... %s (%d)\n", pmsg, err);
+        iret = 1;
+    }
+    return iret;
+}
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
-    int len;
+    int len, cons;
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // TODO: Place code here.
     set_log_file("tempcon3.txt", 0);
+    cons = attach_console();
     add_sys_time(1);
 
     // Initialize global strings
@@ -65,6 +149,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     }
 
     SPRTF("Ended test-con3 app...\n");
+    if (!cons)
+        sendEnterKey();
+
     return (int) msg.wParam;
 }
 
