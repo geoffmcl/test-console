020151b09c3025370bcc4b1b8954b890b2da1a08
Geoff McLane <ubuntu@geoffair.info>
2018-01-21 04:33:16 +0100

    Add test-con3 Win32 app

---
+++ src/Win32/test-con3.h
@@ -0,0 +1,26 @@
+/*\
+* test-con3.h
+*
+* Copyright (c) 2018 - Geoff R. McLane
+* Licence: GNU GPL version 2
+*
+\*/
+#ifndef _TEST_CON3_H_
+#define _TEST_CON3_H_
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#include "resource.h"
+
+#endif
+/* eof */
